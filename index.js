c21d5e843d2f65602d38e90e1618b1d6a45bb15c
reynaVite <132850547+reynaVite@users.noreply.github.com>
2024-07-17 00:07:26 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');  
 const mysql = require('mysql2/promise');
- 
 const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 
 const PORT = process.env.PORT || 3000;
 
